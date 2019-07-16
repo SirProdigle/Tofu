0cfbea821a6c235ee18e33e4ec9341e8849a2068
Liam <main.prodigle@gmail.com>
2019-07-16 21:39:03 +0100

    fix env loading

--- Jinko/app.js
+++ Jinko/app.js
@@ -1,16 +1,17 @@
+const approot = require("app-root-path");
+require('dotenv').config(`${approot}/.env`); // Needed to read env file
+const config = require('./Config'); //include our Config file as Config
 const express = require('express'),
     http = require('http'), //http for server
     fs = require('fs'), //file system for dynamically reading Controllers
     app = express(),
-    approot = require("app-root-path"),
     morgan = require("morgan"),
     winston = require("winston"),
     Logger = require(approot + "/Engine/Logger");
 
 startup = require(approot + "/Engine/Startup");
 customStartup = require(approot + "/Middleware/Startup");
-require('dotenv').config(); // Needed to read env file
-const config = require('./Config'); //include our Config file as Config
+
 
 //Run through all the startup functions defined in Startup/System/Startup.js, they can only take in app as a param for this
 
