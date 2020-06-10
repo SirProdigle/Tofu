0e207e5d8527d341f34ef1f86842148865ad4353
Prodigle <main.prodigle@gmail.com>
2020-06-10 21:35:36 +0100

    Make section of cmdline

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const prog = require('caporal');
 const createCmd = require('./lib/create');
 const enginesCmd = require('./lib/engines');
 const update = require('./lib/update');
+const makeCmd = require('./lib/make');
 
 prog
     .version('0.0.1')
@@ -16,7 +17,14 @@ prog
     .action(enginesCmd)
     .command("update", "Update a project to the latest Tofu template version")
     .option("--dry-run", "Show changes an update would make")
-    .action(()=>{console.error("Update feature not yet implemented".red)});
+    .action(()=>{console.error("Update feature not yet implemented".red)})
+    .command("make", "Generate an MVC component",)
+    .argument("<name>","Prefix for Controller/Middleware (e.g User => UserController)")
+    .argument('[all|controller|model|middleware]...', "Type of unit to generate",/^controller|model|middleware|all$/,
+        "all")
+    .option("--crud","Create boilerplate CRUD functionality")
+    .action(makeCmd)
+    ;
 
 
 prog.parse(process.argv);
