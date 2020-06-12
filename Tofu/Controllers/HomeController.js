174296caf89ce40fc966637a6e44527a87e367f3
Prodigle <main.prodigle@gmail.com>
2020-06-12 17:47:57 +0100

    base home controller

---
+++ Tofu/Controllers/HomeController.js
@@ -0,0 +1,16 @@
+const appRoot = require("app-root-path");
+const Controller = require(`${appRoot}/Engine/Controller`);
+       
+    class HomeController extends Controller {
+        constructor() {
+            super();
+            
+            this.router.get('/', (req, res) => {
+                res.send("Welcome to Tofu!");
+            });
+        
+        }
+    }
+            
+const Con = new HomeController();
+module.exports = Con.router;
