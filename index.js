d4e82d62eefdeffb371acb0a4f9d4dcde7c5b206
Prodigle <main.prodigle@gmail.com>
2020-06-12 17:06:10 +0100

    Nice console logging for CLI

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ const update = require('./lib/update');
 const makeCmd = require('./lib/make');
 const setCmd = require("./lib/set");
 const fs = require("fs");
+const logger = require("./lib/Logger");
 
 prog
     .version('0.0.1')
+    .logger(logger)
     .command('create', 'Create a new application')
     .argument("<name>", "Name of the project being created")
     .option("--no-examples","Create Tofu project without examples") //Options are checked as options['noExamples']
-    .argument("[viewEngine]", "View engine being used (Tofu engines)", false)
+    .argument("[viewEngine]", "View engine being used (tofu show engines for full list)", false)
     .action(createCmd)
     .command("show engines", "Show list of supported view engines")
     .action(enginesCmd)
-    .command("update", "Update a project to the latest Tofu template version")
+    .command("upgrade", "Update a project to the latest Tofu template version")
     .option("--dry-run", "Show changes an update would make")
     .action(()=>{console.error("Update feature not yet implemented".red)})
     .command("make", "Generate an MVC component",)
