ed3cc76c08f63af87631dacbe059b79d6d3aedb3
Prodigle <main.prodigle@gmail.com>
2021-02-21 23:30:19 +0000

    gulp stuff

--- Tofu/Config/express.js
+++ Tofu/Config/express.js
@@ -1,7 +1,7 @@
 const appRoot = require("app-root-path");
 const path = require("path");
 module.exports = {
-  port: process.env.PORT || "80",
+  port: process.env.PORT || "8000",
   publicFolder: appRoot + '/Public',
   forceHTTPS: process.env.FORCE_HTTPS || false,
   viewDir: appRoot + '/Resources/Views',
---
+++ Tofu/gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require("gulp"),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	jshint = require("gulp-jshint"),
+	sass = require("gulp-dart-sass"),
+	cssnano = require("gulp-cssnano"),
+	imagemin = require("gulp-imagemin"),
+	cache = require("gulp-cache"),
+	del = require("del"),
+	runSequence = require("run-sequence"),
+	config = require("./Config"),
+	sourcemaps = require("gulp-sourcemaps"),
+	root = require("app-root-path")
+
+gulp.task("js", () =>
+{
+	return gulp.src(root +'/Resources/JS/*.js')
+		.pipe(sourcemaps.init({largeFile: true}))
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(uglify())
+		.pipe(concat('bundle.js'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(root +'/Public/JS'));
+})
+
+gulp.task("sass", () => {
+	return gulp.src(root +'/Resources/{SASS,CSS}/*.{css,sass,scss}')
+		.pipe(sass())
+		.pipe(cssnano())
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(root +"/Public/CSS"))
+})
+
+gulp.task("images", () => {
+	return gulp.src(root +"/Resources/Images/*.{png,jpg,gif,svg}")
+		.pipe(cache(imagemin({interlaced: true})))
+		.pipe(gulp.dest(root +"/Public/Images"))
+})
+gulp.task("clean", async() => {
+	del.sync("Public")
+})
+
+gulp.task("default", gulp.series(gulp.series("clean"),gulp.parallel("sass", "images","js")))
